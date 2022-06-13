3049c3fe3fd6e9aead98944607e7d6cf7511886f
laflafPow <tuzhilaflaf@gmail.com>
2022-06-13 16:49:41 +0300

    Регистрация до предпочтений --------------------------- 1. Реализовать добавление предпочтений 2. Реализовать добавление фото 3. Валидация при регистрации на типы данных 4. Поиск 5. Изменение существующих настроек 6. Украшения, эмодзи и т.п. (необязательно)

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 # в этом файле будут храниться все методы для взаимодействия с бд
 
 import sqlite3
+
 import telebot
 
 # Подключаем бота
@@ -13,9 +14,46 @@ bot = telebot.TeleBot(token)
 conn = sqlite3.connect('db/UserDatabase.db', check_same_thread=False)
 cursor = conn.cursor()
 
+
 # Метод для внесения значений в таблицу User и сохранения изменений
 
-def db_user_val(UserName: str, Age: int, GenderID: int, CityID: int):
-    cursor.execute('INSERT INTO test (UserName, Age, GenderID, CityID) VALUES (?, ?, ?, ?)',
-                   (UserName, Age, GenderID, CityID))
+def db_user_val(UserName: str, Age: int, GenderID: int, City: str, MaleSearchID: int, UserDescription: str):
+    cursor.execute(
+        'INSERT INTO User (UserName, Age, GenderID, City, MaleSearchID, UserDescription) VALUES (?, ?, ?, ?, ?, ?)',
+        (UserName, Age, GenderID, City, MaleSearchID, UserDescription))
     conn.commit()
+
+
+# Метод внесения данных в таблицу Предпочтений(prefere)
+
+def db_prefer_val(UserName, FoodName):
+    cursor.execute('SELECT UserID From User Where UserName =?', UserName)  # Конвертируем UserName в UserID
+    UserID = cursor.fetchone()
+    UserID = UserID[1]
+
+    cursor.execute('SELECT FoodID From User Where FoodName =?', FoodName)
+    FoodID = cursor.fetchone()
+    FoodID = FoodID[1]
+
+    cursor.execute('INSERT INTO prefere (UserID, FoodID) VALUES (?, ?)',
+                   (UserID, FoodID))
+    conn.commit()
+
+
+# -----------------------------------------------------------------------------
+
+# Метод конвертирования картинки в бинарный вид
+def img_bin(img):
+    f = open(img, "rb")
+    imgbin = f.read
+    return imgbin
+
+
+# Метод добавления понравившихся пользователей
+def Like(Who, Whom):
+    cursor.execute(
+        'INSERT INTO UserLike (WhoLiked, WhomLiked) VALUES (?, ?)',
+        (Who, Whom))
+    conn.commit()
+
+##asdasdasd
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import telebot
 from telebot import types
 import database
 import userInteraction
-import emoji
 
 # в файле database будут храниться все методы для взаимодействия с бд
 
@@ -12,69 +11,108 @@ token = '5270245996:AAEDYl1qVn02R-JFxF5HhrJ47RQwEItT3ww'
 bot = telebot.TeleBot(token)
 
 # данные пользователя
-userName = ''
+UserName = ''
+Age = 0
+GenderID = 0
+City = ''
+MaleSearchID = 0
+UserDescription = ''
+
 
 @bot.message_handler(commands='start')
 def start_message(message):
     rmk = types.ReplyKeyboardMarkup()
-    rmk.add(types.KeyboardButton(emoji.emojize(':OK_hand:')))
+    rmk.add(types.KeyboardButton('ОК'))
 
-    msg = bot.send_message(message.chat.id, 'Привет! Я помогу найти'
+    bot.send_message(message.chat.id, 'Привет! Я помогу найти'
                                       ' тебе человека с которым '
                                       'ты сможешь хорошо провести время,'
                                       ' т.е. покушать, но для начала'
                                       ' расскажи чутка о себе,'
                                       ' чтобы я нашел подходящего человека...', reply_markup=rmk)
 
-    bot.register_next_step_handler(msg, get_name)
+    bot.register_next_step_handler(message, welcome)
 
-def get_name(message):
+def welcome(message):
     rmk = types.ReplyKeyboardRemove()
+
     msg = bot.send_message(message.chat.id, 'Как тебя зовут?', reply_markup=rmk)
+    bot.register_next_step_handler(msg, get_Name)
 
-    if message.content_type != 'text':
-        bot.send_message(message.chat.id, 'Я не думаю что тебя так зовут, попробуем еще раз')
-        bot.register_next_step_handler(msg, get_name)
-    else:
-        global userName
-        print(message.text)
-        userName = msg.text
-        bot.register_next_step_handler(msg, get_age)
-
-def get_age(message):
+def get_Name(message):
+    global UserName
+    UserName = message.text
     msg = bot.send_message(message.chat.id, 'Крутое имя, перейдем дальше, сколько тебе лет?')
+    bot.register_next_step_handler(msg, get_Age)
 
-    bot.register_next_step_handler(msg, get_gender)
+def get_Age(message):
+    global Age
+    while Age == 0:  # проверяем что возраст изменился
+        try:
+            Age = int(message.text)  # проверяем, что возраст введен корректно
+        except ValueError:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
 
-def get_gender(message):
     rmk = types.ReplyKeyboardMarkup()
     rmk.add(types.KeyboardButton('Мужской'), types.KeyboardButton('Женский'))
 
     msg = bot.send_message(message.chat.id, 'Укажи свой пол', reply_markup=rmk)
+    bot.register_next_step_handler(msg, get_Gender)
 
-    bot.register_next_step_handler(msg, get_genderSearch)
+def get_Gender(message):
+    global GenderID
+    while GenderID == 0:
+        if message.text == 'Мужской':
+            GenderID = 2
+        elif message.text == 'Женский':
+            GenderID = 1
+        else:
+            bot.send_message(message.from_user.id, 'Пол введен некорректно')
 
-
-def get_genderSearch(message):
     rmk = types.ReplyKeyboardMarkup()
     rmk.add(types.KeyboardButton('Мужчин'), types.KeyboardButton('Женщин'), types.KeyboardButton('Всех'))
 
     msg = bot.send_message(message.chat.id, 'Кого будем искать?', reply_markup=rmk)
+    bot.register_next_step_handler(msg, get_MaleSearch)
 
-    bot.register_next_step_handler(msg, get_city)
 
+def get_MaleSearch(message):
+    global MaleSearchID
+    while MaleSearchID == 0:
+        if message.text == 'Мужчин':
+            MaleSearchID = 2
+        elif message.text == 'Женщин':
+            MaleSearchID = 1
+        elif message.text == 'Всех':
+            MaleSearchID = 3
+        else:
+            bot.send_message(message.from_user.id, 'Введено некорректное значение')
 
-def get_city(message):
     rmk = types.ReplyKeyboardRemove()
 
     msg = bot.send_message(message.chat.id, 'Укажите город!', reply_markup=rmk)
+    bot.register_next_step_handler(msg, get_City)
+
+def get_City(message):
+    global City
+    while City == '':
+        try:
+            int(message.text)
+            bot.send_message(message.chat.id, 'Некорректное значение')
+        except ValueError:
+            try:
+                City = str(message.text)
+            except ValueError:
+                bot.send_message(message.chat.id, 'Некорректное значение')
+
+    msg = bot.send_message(message.chat.id, 'Расскажи что-то о себе. Это повысит шанс на взаимную симпатию:)')
     bot.register_next_step_handler(msg, get_userDescription)
 
 def get_userDescription(message):
-    msg = bot.send_message(message.chat.id, 'Расскажи что-то о себе. Это повысит шанс на взаимную симпатию:)')
-    bot.register_next_step_handler(msg, get_userPrefer)
+    global UserDescription
+    UserDescription = message.text
+    database.db_user_val(UserName, Age, GenderID, City, MaleSearchID, UserDescription)
 
-def get_userPrefer(message):
     rmk = types.ReplyKeyboardMarkup()
     rmk.add(types.KeyboardButton('Сохранить'))
 
